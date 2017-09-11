33943212cd80388008ac403871c2b91259e72590
AubreyBellis <contactaubreybellis@gmail.com>
2017-09-11 19:51:14 -0400

    updating routes

--- app/controllers/api/teachers_controller.rb
+++ app/controllers/api/teachers_controller.rb
@@ -3,6 +3,27 @@ class Api::TeachersController < ApplicationController
         @teachers = Teacher.all
         render json: @teachers
     end
+    def create
+      @teacher = Teacher.create!(teacher_params)
+      redirect_to teacher_path(@teacher)
+    end
+  
+    def show
+      @teacher = Teacher.find(params[:id])
+      render json: @teacher
+    end
+  
+    def update
+      @teacher = Teacher.find(params[:id])
+      @teacher.update!(teacher_params)
+      redirect_to teacher_path(@teacher)
+    end
+  
+    def destroy
+      @teacher = Teacher.find(params[:id])
+      @teacher.destroy
+      redirect_to teachers_path
+    end
     private
     
       def teacher_params
