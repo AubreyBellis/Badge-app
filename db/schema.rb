24117bf595fef8f44ea0561f5c1901f924003f00
AubreyBellis <contactaubreybellis@gmail.com>
2017-09-26 17:55:07 -0400

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170917223942) do
+ActiveRecord::Schema.define(version: 20170915151524) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,7 +52,6 @@ ActiveRecord::Schema.define(version: 20170917223942) do
     t.datetime "updated_at", null: false
     t.string "image"
     t.string "title"
-    t.string "api_id"
   end
 
   add_foreign_key "classrooms", "teachers"
